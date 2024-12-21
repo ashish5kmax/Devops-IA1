1437323227fcf40caf8c334eaf9e3bbd0861dd9e
Ashish Kumar Sahoo <79834663+ashish5kmax@users.noreply.github.com>
2024-12-21 14:45:32 +0530

    Update factorial.py

--- src/factorial.py
+++ src/factorial.py
@@ -1,6 +1,10 @@
 def factorial(n):
   if(n==0) return 1
-  return n*factorial(n-1)
+  fact = 1
+  for i in range(1,n+1):
+    fact*=i
+  
+  return fact
 
 def test_factorial():
   assert factorial(5) == 120
